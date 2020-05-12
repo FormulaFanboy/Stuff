4b77e4afe94648eac241e2ed14a02d981419db1c
FormulaFanboy <65199262+FormulaFanboy@users.noreply.github.com>
2020-05-11 22:32:27 -0400

    Update localestorage.js

--- localestorage.js
+++ localestorage.js
@@ -1,31 +1,18 @@
-new (function() {
-	var ext = this;
-	var descriptor = {
-		blocks: [
+// Core, Team, and Official extensions can `require` VM code:
+const ArgumentType = require('../../extension-support/argument-type');
+const BlockType = require('../../extension-support/block-type');
+
+class SomeBlocks {
+    // ...
+    getInfo () {
+        return {
+            id: 'someBlocks',
+            name: 'Some Blocks',
+            [
 			['r', '%s', 'readLocalStorage', 'highscore'],
 			[' ', 'set %s to %s', 'setLocalStorage', 'highscore', '12'],
-		],
-		url : 'https://jgames101.github.io/scratch-extensions/localstorage.html'
-	};
-	ext._shutdown = function() {
-		
-	};
-	
-	ext._getStatus = function() {
-    if (window.localStorage) {
-		  return {status:2, msg:'LocalStorage supported by your browser!'};
-    } else {
-      return {status:0, msg:'LocalStorage is not supported by your browser. Try recent versions of Chrome or Firefox.'};
-    };
-	};
-  
-	ext.readLocalStorage = function(name) {
-		return localStorage.getItem(name);
-	};
-	
-	ext.setLocalStorage = function(name, value) {
-		return localStorage.setItem(name, value);
-	};
-
-	ScratchExtensions.register('LocalStorage Variables', descriptor, ext);
-})();
+		]
+        };
+    }
+    // ...
+}
